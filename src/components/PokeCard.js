916b76221f8ca99ed8a2b0fe244b04081cf946ad
Enrique Del Castillo Chávez <ecch9210@gmail.com>
2021-10-05 15:48:58 -0500

    Finished images

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -4,12 +4,14 @@ import { mockPokemonData } from "../mock/pokeData";
 export default function PokeCard() {
   return (
     <div>
-      <ul>
-        <li>{mockPokemonData.name}</li>
-        <li>{mockPokemonData.sprites.front_default}</li>
-        <li>{mockPokemonData.sprites.front_shiny}</li>
-        <li>{mockPokemonData.video}</li>
-      </ul>
+      <h1>{mockPokemonData.name}</h1>
+
+      <img src={mockPokemonData.sprites.front_shiny} alt="pic" />
+      <img src={mockPokemonData.sprites.front_default} alt="pic" />
+
+      <p>
+        <a href={mockPokemonData.video}></a>
+      </p>
     </div>
   );
 }
